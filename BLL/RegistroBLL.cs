db499d4ef3de5e74b71eefea17dc3095b93057a8
Harlit <harlit.estevez@gmail.com>
2022-10-03 18:36:19 -0400

    se cambia nombre models

--- BLL/RegistroBLL.cs
+++ BLL/RegistroBLL.cs
@@ -1,63 +1,63 @@
-using System.Linq.Expressions;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
+// using System.Linq.Expressions;
+// using Microsoft.EntityFrameworkCore;
+// using System.Linq;
 
-public class RegistrosBLL
-{
-    private Contexto _contexto;
+// public class RegistrosBLL
+// {
+//     private Contexto _contexto;
 
-    public RegistrosBLL(Contexto contexto)
-    {
-        _contexto = contexto;
-    }
+//     public RegistrosBLL(Contexto contexto)
+//     {
+//         _contexto = contexto;
+//     }
 
-    public bool Existe(int registroId)
-    {
-        return _contexto.Registros
-        .Any (o => o.RegistroId == registroId);
-    }
+//     public bool Existe(int registroId)
+//     {
+//         return _contexto.Registros
+//         .Any (o => o.RegistroId == registroId);
+//     }
 
-    public bool Insertar(Registros registro)
-    {
-        _contexto.Registros.Add(registro);
-        return _contexto.SaveChanges() > 0;
-    }
+//     public bool Insertar(Registros registro)
+//     {
+//         _contexto.Registros.Add(registro);
+//         return _contexto.SaveChanges() > 0;
+//     }
 
-    public bool Modificar(Registros registro)
-    {
-        _contexto.Entry(registro).State = EntityState.Modified;
-        return _contexto.SaveChanges() > 0;
-    }
+//     public bool Modificar(Registros registro)
+//     {
+//         _contexto.Entry(registro).State = EntityState.Modified;
+//         return _contexto.SaveChanges() > 0;
+//     }
 
-    public bool Guardar(Registros registro)
-    {
-        if (!Existe(registro.RegistroId))
-        return this.Insertar(registro);
-        else
-        {
-            return this.Modificar(registro);
-        }
-    }
+//     public bool Guardar(Registros registro)
+//     {
+//         if (!Existe(registro.RegistroId))
+//         return this.Insertar(registro);
+//         else
+//         {
+//             return this.Modificar(registro);
+//         }
+//     }
 
-    public bool Eliminar(Registros registro)
-    {
-        _contexto.Entry(registro).State = EntityState.Deleted;
-        return _contexto.SaveChanges() > 0;
-    }
+//     public bool Eliminar(Registros registro)
+//     {
+//         _contexto.Entry(registro).State = EntityState.Deleted;
+//         return _contexto.SaveChanges() > 0;
+//     }
 
-    public Registros? Buscar(int registroId)
-    {
-        return _contexto.Registros
-        .Where (o => o.RegistroId == registroId)
-        .AsNoTracking()
-        .SingleOrDefault();
-    }
+//     public Registros? Buscar(int registroId)
+//     {
+//         return _contexto.Registros
+//         .Where (o => o.RegistroId == registroId)
+//         .AsNoTracking()
+//         .SingleOrDefault();
+//     }
 
-    public List<Registros> GetList(Expression<Func<Registros,bool>> Criterio)
-    {
-        return _contexto.Registros
-        .AsNoTracking()
-        .Where(Criterio)
-        .ToList();
-    }
-}
+//     public List<Registros> GetList(Expression<Func<Registros,bool>> Criterio)
+//     {
+//         return _contexto.Registros
+//         .AsNoTracking()
+//         .Where(Criterio)
+//         .ToList();
+//     }
+// }
